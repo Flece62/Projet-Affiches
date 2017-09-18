591154c5c4aa93e264a1d9996069d2689588259b
Jason-Darwnin <31890092+Jason-Darwnin@users.noreply.github.com>
2017-09-18 15:12:59 +0200

    Connexion

---
+++ Projet-Affiches/Connexion.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Connexion
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuPrincipale = New System.Windows.Forms.MenuStrip()
+        Me.MenuPAccueil = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuPAffiaches = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuPAdherents = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuPContact = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.MenuPrincipale.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 98)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(322, 313)
+        Me.Panel1.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 122)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Mot de passe :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Adresse e-mail :"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(17, 148)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(277, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(17, 82)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(277, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(159, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Se connecter avec son compte "
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.TextBox5)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.TextBox4)
+        Me.Panel2.Controls.Add(Me.TextBox3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Location = New System.Drawing.Point(356, 98)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(322, 313)
+        Me.Panel2.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 186)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Mot de passe :"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(25, 212)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(277, 20)
+        Me.TextBox5.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 56)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Pseudo"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Adresse e-mail :"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(25, 148)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(277, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(25, 82)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(277, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(137, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Créer un compte utilisateurs"
+        '
+        'MenuPrincipale
+        '
+        Me.MenuPrincipale.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.MenuPrincipale.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.MenuPrincipale.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuPAccueil, Me.MenuPAffiaches, Me.MenuPAdherents, Me.MenuPContact})
+        Me.MenuPrincipale.Location = New System.Drawing.Point(0, 0)
+        Me.MenuPrincipale.Name = "MenuPrincipale"
+        Me.MenuPrincipale.Size = New System.Drawing.Size(690, 27)
+        Me.MenuPrincipale.TabIndex = 14
+        Me.MenuPrincipale.Text = "MenuStrip1"
+        '
+        'MenuPAccueil
+        '
+        Me.MenuPAccueil.Name = "MenuPAccueil"
+        Me.MenuPAccueil.Size = New System.Drawing.Size(106, 23)
+        Me.MenuPAccueil.Text = "MenuPAccueil"
+        '
+        'MenuPAffiaches
+        '
+        Me.MenuPAffiaches.Name = "MenuPAffiaches"
+        Me.MenuPAffiaches.Size = New System.Drawing.Size(118, 23)
+        Me.MenuPAffiaches.Text = "MenuPAffiaches"
+        '
+        'MenuPAdherents
+        '
+        Me.MenuPAdherents.Name = "MenuPAdherents"
+        Me.MenuPAdherents.Size = New System.Drawing.Size(127, 23)
+        Me.MenuPAdherents.Text = "MenuPAdherents"
+        '
+        'MenuPContact
+        '
+        Me.MenuPContact.Name = "MenuPContact"
+        Me.MenuPContact.Size = New System.Drawing.Size(113, 23)
+        Me.MenuPContact.Text = "MenuPContact"
+        '
+        'Connexion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(690, 433)
+        Me.Controls.Add(Me.MenuPrincipale)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Connexion"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.MenuPrincipale.ResumeLayout(False)
+        Me.MenuPrincipale.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents MenuPrincipale As MenuStrip
+    Friend WithEvents MenuPAccueil As ToolStripMenuItem
+    Friend WithEvents MenuPAffiaches As ToolStripMenuItem
+    Friend WithEvents MenuPAdherents As ToolStripMenuItem
+    Friend WithEvents MenuPContact As ToolStripMenuItem
+End Class
---
+++ Projet-Affiches/Connexion.vb
@@ -0,0 +1,9 @@
+﻿Public Class Connexion
+    Private Sub Connexion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MenuPAccueil.Text = "Accueil"
+        MenuPAffiaches.Text = "Les affiches"
+        MenuPAdherents.Text = "Les adhérents"
+        MenuPContact.Text = "Contact"
+    End Sub
+
+End Class
